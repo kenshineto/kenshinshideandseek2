2d73e1f9751effdd86acc6fb20151353087d1206
Freya Murphy <freya@freyacat.org>
2026-03-31 18:59:48 -0400

    lsp fixed itself?

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,34 +32,3 @@ subprojects {
 		}
 	}
 }
-
-// the following is to fix issues with some kotlin LSPs
-// they are too dumb to look in the build.gradle.kts files
-// for each sub project, i am no sure why
-// this should not matter that this is duplicated here
-// ... i dont like it ... but I dont know a better solution
-
-repositories {
-	maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
-	maven("https://repo.codemc.io/repository/maven-releases/")
-}
-
-dependencies {
-    compileOnly("org.spigotmc:spigot-api:26.1-R0.1-SNAPSHOT")
-	compileOnly("com.github.retrooper:packetevents-spigot:2.11.2")
-	implementation("com.github.cryptomorin:XSeries:13.6.0")
-
-	// core libs
-	compileOnly("org.yaml:snakeyaml:2.6")
-	implementation(kotlin("reflect"))
-
-	// orm
-	implementation("org.jetbrains.exposed:exposed-core:1.1.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:1.1.1")
-
-	// database
-	compileOnly("org.xerial:sqlite-jdbc:3.51.3.0")
-	implementation("com.mysql:mysql-connector-j:9.6.0")
-	implementation("org.postgresql:postgresql:42.7.10")
-	implementation("com.zaxxer:HikariCP:4.0.3")
-}
