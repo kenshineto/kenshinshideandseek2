1bb91c79553a904aba728865f538404520dba5ad
Freya Murphy <freya@freyacat.org>
2026-04-03 14:40:38 -0400

    switch to auto toolchain download for jdk8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "KenshinsHideAndSeek"
 
 include("core", "bukkit", "fabric")
