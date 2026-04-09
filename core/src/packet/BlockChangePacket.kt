a53048a365816a3800e1df0fc0c703df6aec8179
Freya Murphy <freya@freyacat.org>
2026-04-09 11:32:32 -0400

    refactor block change packet

--- core/src/packet/BlockChangePacket.kt
+++ core/src/packet/BlockChangePacket.kt
@@ -9,7 +9,10 @@ import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerBl
 import kotlin.math.floor
 
 data class BlockChangePacket(val location: Location, val material: Material) {
-    fun send(player: Player) {
+
+    private val packet: WrapperPlayServerBlockChange? = getPacket()
+
+    private fun getPacket(): WrapperPlayServerBlockChange? {
         val vector =
             Vector3i(
                 floor(location.x).toInt(),
@@ -18,19 +21,23 @@ data class BlockChangePacket(val location: Location, val material: Material) {
             )
 
         val key = material.key
-        val packet =
-            when {
-                key.minecraftKey != null -> {
-                    val state = StateTypes.getByName(key.minecraftKey)?.createBlockState() ?: return
-                    WrapperPlayServerBlockChange(vector, state)
-                }
-                key.minecraftId != null -> {
-                    val id = key.minecraftId shl 4
-                    WrapperPlayServerBlockChange(vector, id)
-                }
-                else -> return
-            }
 
+        return when {
+            key.minecraftKey != null -> {
+                val state =
+                    StateTypes.getByName(key.minecraftKey)?.createBlockState() ?: return null
+                WrapperPlayServerBlockChange(vector, state)
+            }
+            key.minecraftId != null -> {
+                val id = key.minecraftId shl 4
+                WrapperPlayServerBlockChange(vector, id)
+            }
+            else -> return null
+        }
+    }
+
+    fun send(player: Player) {
+        val packet = packet ?: return
         player.sendPacket(packet)
     }
 }
