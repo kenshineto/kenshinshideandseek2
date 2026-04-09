f1d52758ddb769bcfaaf037b0a332b0f64c67e01
Freya Murphy <freya@freyacat.org>
2026-04-09 08:37:05 -0400

    dont let spectators interact with the world

--- bukkit/src/event/InteractListener.kt
+++ bukkit/src/event/InteractListener.kt
@@ -7,6 +7,7 @@ import cat.freya.khs.event.InteractEvent
 import cat.freya.khs.event.UseEvent
 import cat.freya.khs.event.onInteract
 import cat.freya.khs.event.onUse
+import org.bukkit.event.Event
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -19,30 +20,43 @@ class InteractListener(val plugin: KhsPlugin) : Listener {
         plugin.server.pluginManager.registerEvents(this, plugin)
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    fun onPlayerInteract(event: PlayerInteractEvent) {
+    private fun handleUse(event: PlayerInteractEvent): Boolean {
         val bukkitPlayer = event.player
+        val item = BukkitItem.wrap(event.item) ?: return false
 
         val khsPlayer = BukkitPlayer(plugin, bukkitPlayer)
-
-        val block = event.clickedBlock?.type?.name
-        if (event.action == Action.RIGHT_CLICK_BLOCK && block != null) {
-            val khsEvent = InteractEvent(plugin.khs, khsPlayer, block)
-            onInteract(khsEvent)
-
-            if (khsEvent.cancelled) {
-                event.isCancelled = true
-                return
-            }
-        }
-
-        val item = BukkitItem.wrap(event.item) ?: return
         val khsEvent = UseEvent(plugin.khs, khsPlayer, item)
         onUse(khsEvent)
 
-        if (khsEvent.cancelled) {
+        return khsEvent.cancelled
+    }
+
+    private fun handleInteract(event: PlayerInteractEvent): Boolean {
+        val bukkitPlayer = event.player
+        val block = event.clickedBlock?.type?.name
+        val action =
+            when (event.action) {
+                Action.LEFT_CLICK_BLOCK -> InteractEvent.Action.LEFT_CLICK
+                Action.LEFT_CLICK_AIR -> InteractEvent.Action.LEFT_CLICK
+                Action.RIGHT_CLICK_BLOCK -> InteractEvent.Action.RIGHT_CLICK
+                Action.RIGHT_CLICK_AIR -> InteractEvent.Action.RIGHT_CLICK
+                Action.PHYSICAL -> InteractEvent.Action.ATTACK
+            }
+
+        val khsPlayer = BukkitPlayer(plugin, bukkitPlayer)
+        val khsEvent = InteractEvent(plugin.khs, khsPlayer, action, block)
+        onInteract(khsEvent)
+
+        return khsEvent.cancelled
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val cancelled = handleUse(event) || handleInteract(event)
+
+        if (cancelled) {
             event.isCancelled = true
-            return
+            event.setUseInteractedBlock(Event.Result.DENY)
         }
     }
 }
--- core/src/event/onInteract.kt
+++ core/src/event/onInteract.kt
@@ -3,13 +3,31 @@ package cat.freya.khs.event
 import cat.freya.khs.Khs
 import cat.freya.khs.world.Player
 
-data class InteractEvent(val plugin: Khs, val player: Player, val block: String) : Event()
+data class InteractEvent(
+    val plugin: Khs,
+    val player: Player,
+    val action: Action,
+    val block: String?,
+) : Event() {
+    enum class Action {
+        LEFT_CLICK,
+        RIGHT_CLICK,
+        ATTACK,
+    }
+}
 
 fun onInteract(event: InteractEvent) {
-    val (plugin, player, block) = event
+    val (plugin, player, _, block) = event
     val game = plugin.game
 
     if (!game.hasPlayer(player)) return
+
+    // seekers are not allowed to interact
+    // with anything
+    if (game.isSpectator(player)) {
+        event.cancel()
+        return
+    }
 
     if (plugin.config.blockedInteracts.any { it.equals(block, ignoreCase = true) }) {
         // this interaction is blocked!
